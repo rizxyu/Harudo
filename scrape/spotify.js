d561618f9cc866a9b4373685f9e5066d9992ec32
J <88314302+carlxjoe@users.noreply.github.com>
2022-10-23 13:10:56 +0700

    Create spotify.js

---
+++ scrape/spotify.js
@@ -0,0 +1,11 @@
+/**
+*
+* Made by Rizky
+* Gunakan dengan bijak
+* Gw enskrip
+**/
+
+//Versi ESM , mksdnya dibagian modul pake import export
+
+
+// Versi Biasa , module.export bukan import export
